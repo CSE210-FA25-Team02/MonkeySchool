bdc45d174e0987dcff00c87ffaf216dae631a3c6
indreshp135 <indreshp135@gmail.com>
2025-11-17 21:25:26 -0800

    fix: lint

--- code/src/utils/html-templates.js
+++ code/src/utils/html-templates.js
@@ -108,7 +108,7 @@ export function createErrorMessage(message, errors = null) {
               .map(
                 (error) => `
                 <li class="alert__item">${escapeHtml(error)}</li>
-            `
+            `,
               )
               .join("")}
         </ul>
