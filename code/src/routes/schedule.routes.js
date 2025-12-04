4d3325c04670d96a5c7e4d82dad576eafc326ade
Shravya Ramasahayam <58300338+shravya-R@users.noreply.github.com>
2025-12-03 18:40:33 -0800

    Feature: Calendar view for course (#87)

--- code/src/routes/schedule.routes.js
+++ code/src/routes/schedule.routes.js
@@ -5,6 +5,7 @@
 
 import { Router } from "express";
 import * as scheduleController from "../controllers/schedule.controller.js";
+import * as eventController from "../controllers/event.controller.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { requireAuth } from "../middleware/auth.js";
 
@@ -15,6 +16,41 @@ router.get(
   "/classes/:id/schedule",
   requireAuth,
   asyncHandler(scheduleController.renderClassSchedule),
+);
+
+// Create event
+router.post(
+  "/events/create",
+  requireAuth,
+  asyncHandler(eventController.createEvent),
+);
+
+// Get event by ID
+router.get(
+  "/events/:id",
+  requireAuth,
+  asyncHandler(eventController.getEvent),
+);
+
+// Get event edit form
+router.get(
+  "/events/:id/edit",
+  requireAuth,
+  asyncHandler(eventController.getEventEditForm),
+);
+
+// Update event
+router.put(
+  "/events/:id",
+  requireAuth,
+  asyncHandler(eventController.updateEvent),
+);
+
+// Delete event
+router.delete(
+  "/events/:id",
+  requireAuth,
+  asyncHandler(eventController.deleteEvent),
 );
 
 export default router;
